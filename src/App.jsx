2ffa1cd7b0dc1aeb6c13bb932f279c8da6b4c10b
Dbsshrestha <155275145+Dbsshrestha@users.noreply.github.com>
2024-04-21 22:44:13 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Main from './pages/Main';
 import Another from './pages/Another';
 import './App.css';
 import Userdashboard from './pages/Userdashboard';
+import { gapi } from 'gapi-script';
 
 function App() {
   return (
@@ -30,6 +31,12 @@ function App() {
       </div>
     </Router>
   );
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId: "565407833235-tqv9nd8efn1v8gosqmr2vndh4nkbfgli.apps.googleusercontent.com",
+      plugin_name: "chat"
+    });
+  });
 }
 
 export default App;
