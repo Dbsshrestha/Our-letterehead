5e133808bf8e0e52ded2578da7bb839d3ca3ebdb
Dbsshrestha <155275145+Dbsshrestha@users.noreply.github.com>
2024-04-22 14:17:30 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,49 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Header2 from './Components/Header2/Header2';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Letterhead from './pages/Letterhead';
 import Main from './pages/Main';
 import Another from './pages/Another';
 import './App.css';
-// import Userdashboard from './pages/Userdashboard';
-// import { gapi } from 'gapi-script';
+//import Userdashboard from './pages/Userdashboard';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute'; 
+//import { gapi } from 'gapi-script';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-        <div className="login-container">
-          <Routes>
-            <Route path="/letterhead" element={<Letterhead />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/another" element={<Another />} />
-          {/* <Route path="/userdashboard" element={<Userdashboard/>} /> */}
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
-  // gapi.load("client:auth2", () => {
-  //   gapi.client.init({
-  //     clientId: "565407833235-tqv9nd8efn1v8gosqmr2vndh4nkbfgli.apps.googleusercontent.com",
-  //     plugin_name: "chat"
-  //   });
-  // });
+};
+
+function AppContent() {
+  const location = useLocation();
+  return (
+    <>
+      {['/another', '/main'].includes(location.pathname) ? <Header2 /> : <Header />}
+      <div className="login-container">
+        <Routes>
+          <Route path="/" element={<Letterhead />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/another" element={<Another />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
+/* gapi.load("client:auth2", () => {
+  gapi.client.init({
+    clientId: "565407833235-tqv9nd8efn1v8gosqmr2vndh4nkbfgli.apps.googleusercontent.com",
+    plugin_name: "chat"
+  });
+});
+ */
 export default App;
