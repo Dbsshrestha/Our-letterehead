4200eb0d8b10f105750e05fa1ff7dffe34b914a8
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-22 11:00:54 +0545

    fix: header structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import Letterhead from './pages/Letterhead';
 import Main from './pages/Main';
 import Another from './pages/Another';
 import './App.css';
-import Userdashboard from './pages/Userdashboard';
-import { gapi } from 'gapi-script';
+// import Userdashboard from './pages/Userdashboard';
+// import { gapi } from 'gapi-script';
 
 function App() {
   return (
@@ -26,17 +26,17 @@ function App() {
         <Routes>
           <Route path="/main" element={<Main />} />
           <Route path="/another" element={<Another />} />
-          <Route path="/userdashboard" element={<Userdashboard/>} />
+          {/* <Route path="/userdashboard" element={<Userdashboard/>} /> */}
         </Routes>
       </div>
     </Router>
   );
-  gapi.load("client:auth2", () => {
-    gapi.client.init({
-      clientId: "565407833235-tqv9nd8efn1v8gosqmr2vndh4nkbfgli.apps.googleusercontent.com",
-      plugin_name: "chat"
-    });
-  });
+  // gapi.load("client:auth2", () => {
+  //   gapi.client.init({
+  //     clientId: "565407833235-tqv9nd8efn1v8gosqmr2vndh4nkbfgli.apps.googleusercontent.com",
+  //     plugin_name: "chat"
+  //   });
+  // });
 }
 
 export default App;
